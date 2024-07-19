4ce09c402114cd93d604d8bba492da8a83a932f0
kylnan <kyl3zyl3@gmail.com>
2024-07-18 22:53:00 -0700

    implemented Rotor Class

--- src/Rotor.java
+++ src/Rotor.java
@@ -1,10 +1,10 @@
 public class Rotor {
-    private String wiring; // rotor sets I-V
-    private int position; // gives current index/position of the rotor
-    private int notch; // The notch position that sets off the next rotor found on https://en.wikipedia.org/wiki/Enigma_machine#Rotors
+    private String wiring;  // rotor sets I-V
+    private int position;   // gives current index/position of the rotor
+    private int notch;      // The notch position that sets off the next rotor found on https://en.wikipedia.org/wiki/Enigma_machine#Rotors
 
     // Constructor
-    public Rotor(String wiring, int position, int notch) {
+    public Rotor(String wiring, int notch) {
         this.wiring = wiring;
         this.position = 0; // setting default position to 0
         this.notch = notch;
@@ -12,6 +12,9 @@ public class Rotor {
 
     // set initial position of the rotor
     public void setPosition(int position) {
+        if (position < 0 || position > 25){
+            throw new IllegalArgumentException("Position must be between 0 and 25");
+        }
         this.position = position;
     }
 
@@ -20,12 +23,38 @@ public class Rotor {
         this.position = (this.position + 1) % 26;
     }
 
+    /*
+        - The input character needs to be adjusted by the rotor's current position.
+        - meaning this shifts the character forward in the alphabet by the number of positions the rotor has advanced
+        - the adjusted character is then substituted using the rotor's wiring
+     */
     public char encodeForward(char input){
+        // convert input character to index
+        int index = input - 'A';
 
+        // Adjust for rotor position
+        index = (index + position) % 26;
+
+        // Substitute using wiring;
+        char substitute = wiring.charAt(index);
+
+        // Adjust back for rotor position
+        int outputIndex = (substitute - 'A' - position + 26) % 26;
+        return (char)(outputIndex + 'A');
     }
 
     public char encodeBackward(char input){
+        // convert input character to index
+        int index = input - 'A';
 
+        // Adjust for rotor position
+        index = (index + position + 26) % 26;
+
+        int wiringIndex = (wiring.indexOf((char) (index + 'A')));
+
+        // Adjust back for rotor position
+        int outputIndex = (wiringIndex - position + 26) % 26;
+        return (char)(outputIndex + 'A');
     }
 
     public int getPosition(){
